84abcaea9c12154d72df8acf96276d7e567687ae
andrea sosio <andrea.sosio@xpeppers.com>
2017-07-21 15:05:24 +0200

    Supported more than 59 minutes

--- lib/timespanner.ex
+++ lib/timespanner.ex
@@ -1,5 +1,17 @@
 defmodule TimeSpanner do
   def from_minutes(m) do
-    "#{m} " <> if (m != 1), do: "minutes", else: "minute"
+    result = ""
+    remaining = m
+
+    result = if (m >= 60), do: result <> "1 hour", else: result
+    result = if (m > 60), do: result <> " ", else: result
+
+    remaining = rem(remaining, 60)
+  
+    if(remaining>0 || result=="") do
+      result = result <> "#{remaining} " <> if (remaining != 1), do: "minutes", else: "minute"
+    end
+
+    result
   end
 end
--- test/timespanner_test.exs
+++ test/timespanner_test.exs
@@ -15,4 +15,11 @@ defmodule TimeSpannerTest do
     assert TimeSpanner.from_minutes(0) == "0 minutes"
   end
 
+  test "works for 61 minutes" do
+    assert TimeSpanner.from_minutes(61) == "1 hour 1 minute"
+  end
+
+  test "works for 60 minutes" do
+    assert TimeSpanner.from_minutes(60) == "1 hour"
+  end
 end
