fb2196d340e9a8098083238647746492eb490753
andrea sosio <andrea.sosio@xpeppers.com>
2017-08-21 14:35:04 +0200

    Refactor (to string methods)

--- lib/timespanner.ex
+++ lib/timespanner.ex
@@ -7,11 +7,13 @@ defmodule TimeSpanner do
   end
 
   defp to_string(hours, minutes) do
-    result = ""
-    result = result <> hours_to_string(hours)
-    result = if (hours > 0 && minutes > 0), do: result <> " ", else: result
-    result = result <> minutes_to_string(minutes)
-    if (result == ""), do: "0 minutes", else: result
+    hours_as_string = hours_to_string(hours)
+    minutes_as_string = minutes_to_string(minutes, hours<1)
+    if(hours_as_string != "" && minutes_as_string != "") do
+      hours_as_string <> " " <> minutes_as_string
+    else
+      hours_as_string <> minutes_as_string
+    end
   end
 
   defp hours_to_string(hours) do
@@ -22,12 +24,11 @@ defmodule TimeSpanner do
     end
   end
   
-  defp minutes_to_string(minutes) do
+  defp minutes_to_string(minutes, only_minutes) do
     case minutes do
-      0 -> ""
+      0 -> if (only_minutes), do: "0 minutes", else: ""
       1 -> "1 minute"
       n -> "#{n} minutes"
     end
   end
-  
 end
