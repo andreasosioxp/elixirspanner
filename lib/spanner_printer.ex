3885b35527d8342093e9daf485c03e72d22008af
Daniele Megna <daniele.megna@xpeppers.com>
2017-12-22 14:31:54 +0100

    Refactoring in SpannerPrinter
    Replaced conditional join_pieces with add_comma_conjuctions function

--- lib/spanner_printer.ex
+++ lib/spanner_printer.ex
@@ -4,8 +4,9 @@ defmodule SpannerPrinter do
     pieces
       |> Enum.map(&(piece_to_string(&1)))
       |> remove_empty
+      |> add_comma_conjuctions
       |> add_and_conjuction
-      |> join_pieces 
+      |> Enum.join(" ")
       |> zero_minutes_if_empty
   end
 
@@ -20,13 +21,17 @@ defmodule SpannerPrinter do
 
   defp remove_empty(list), do: Enum.filter(list, &(&1 != "" ))
 
+  defp add_comma_conjuctions(pieces) when length(pieces) < 3, do: pieces
+  defp add_comma_conjuctions(pieces) do
+    [last | rest] = Enum.reverse(pieces) 
+    rest = Enum.map(rest, fn p -> (p <> ",") end)
+    Enum.reverse([last | rest])
+  end
+
   defp add_and_conjuction(pieces) when length(pieces) < 2, do: pieces
   defp add_and_conjuction(pieces) do
     pieces |> List.replace_at(-1, "and " <> List.last(pieces))
   end
-
-  defp join_pieces(pieces) when length(pieces) == 2, do: Enum.join(pieces, " ")
-  defp join_pieces(pieces), do: Enum.join(pieces, ", ")
 
   defp zero_minutes_if_empty(""), do: "0 minutes"
   defp zero_minutes_if_empty(string), do: string
