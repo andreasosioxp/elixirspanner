1e282cbd2b9a1cec698ed7c9c9211895023824c2
Daniele Megna <megna.dany@gmail.com>
2018-02-13 00:18:13 +0100

    Uglified spanner_printer

--- lib/spanner_printer.ex
+++ lib/spanner_printer.ex
@@ -1,47 +1,30 @@
 defmodule SpannerPrinter do
 
   def pieces_to_string(pieces) do
-    pieces_strings = Enum.map(pieces, fn(p) -> piece_to_string(p) end)
-    pieces_strings = Enum.filter(pieces_strings, fn(v) -> v != "" end)
-    pieces_strings = add_comma_conjuctions(pieces_strings)
-    pieces_strings = add_and_conjuction(pieces_strings)
-    result = Enum.join(pieces_strings, " ")
-
-    if(result == "") do
-      "0 minutes"
-    else
-      result
+    zero_excluded = Enum.filter(pieces, fn({_, value}) -> value != 0 end)
+    pieces = Enum.map(zero_excluded, fn(p) -> piece_to_string(p) end)
+    if length(pieces) >= 2 do
+      pieces = List.replace_at(pieces, -1, "and " <> List.last(pieces))
     end
+    if length(pieces) > 2 do
+      result = Enum.join(pieces, ", ")
+    else
+      result = Enum.join(pieces, " ")
+    end
+    if result == "" do
+      result = "0 minutes"
+    end
+    result
   end
 
   defp piece_to_string(piece) do
     unit_name = elem(piece, 0)
     value = elem(piece, 1)
-
     case value do
       0 -> ""
       1 -> "1 #{unit_name}"
       v -> "#{v} #{unit_name}s"
     end
   end
-
-  defp add_comma_conjuctions(pieces) do
-    if length(pieces) >= 3 do
-      [last | rest] = Enum.reverse(pieces) 
-      rest = Enum.map(rest, fn p -> (p <> ",") end)
-      Enum.reverse([last | rest])
-    else
-      pieces
-    end
-  end
-
-  defp add_and_conjuction(pieces) do
-    if length(pieces) >= 2 do
-      pieces |> List.replace_at(-1, "and " <> List.last(pieces))
-    else
-      pieces
-    end
-  end
-
 
 end
